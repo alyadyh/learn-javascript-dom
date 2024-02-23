bdf1db2f72151f9c00f21a8e434e1e1cc7f84d08
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-23 17:45:02 +0700

    konsep dom selection

---
+++ 1 - DOM Selection/script.js
@@ -0,0 +1,7 @@
+// DOM Selection
+// document.getElementById() -> mengembalikan element
+
+// Format: [datatype] [variable] = document.dom-selection('[Id]');
+// Nama variabel dan Id tidak perlu sama
+
+const judul = document.getElementById('judul');
