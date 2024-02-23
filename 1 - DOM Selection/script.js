faeae1ff0e0181bd227943a50803b478e9442031
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-23 17:48:12 +0700

    selection element by id

--- 1 - DOM Selection/script.js
+++ 1 - DOM Selection/script.js
@@ -5,3 +5,12 @@
 // Nama variabel dan Id tidak perlu sama
 
 const judul = document.getElementById('judul');
+
+judul.style.color = 'red';
+// Outputnya akan sama dengan CSS berikut:
+// #judul {
+//     color: red;
+// }
+
+judul.style.backgroundColor = '#ccc';
+judul.innerHTML = "Alya Dhiya"
