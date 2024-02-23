ea6416d986e6c8969c022d1d0adbc511a31e19c9
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-23 17:57:10 +0700

    selection by tagname & classname

--- 1 - DOM Selection/script.js
+++ 1 - DOM Selection/script.js
@@ -14,3 +14,21 @@ judul.style.color = 'red';
 
 judul.style.backgroundColor = '#ccc';
 judul.innerHTML = "Alya Dhiya"
+
+
+// document.getElementsByTagName() -> mengembalikan HTML Collections
+const p = document.getElementsByTagName('p');
+
+// Kalau mau mengubah style semua elemen p
+for( let i = 0; i < p.length; i++ ) {
+    p[i].style.backgroundColor = 'lightblue';
+}
+
+// Kalau mau mengubah style elemen h1 yang spesifik
+const hSatu = document.getElementsByTagName('h1')[0];
+hSatu.style.fontSize = '50px';
+
+
+// document.getElementsByClassName() -> mengembalikan HTML Collections
+const pSatu = document.getElementsByClassName('p1');
+pSatu[0].innerHTML = 'Ini diubah dari JavaScript';
