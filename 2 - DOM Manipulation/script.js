a5d5c6b58c14da0028faab64a678686194ce0a68
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-24 21:13:53 +0700

    set attribute

--- 2 - DOM Manipulation/script.js
+++ 2 - DOM Manipulation/script.js
@@ -9,6 +9,19 @@
 
 
 // element.style.<propertyCSS>
-const judul = document.querySelector('#judul');
-judul.style.color = 'lightpink';
-judul.style.backgroundColor = 'salmon';
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightpink';
+// judul.style.backgroundColor = 'salmon';
+
+
+
+// element.setAttribute() -> untuk memodifikasi attribute dalam console
+// Class yang ada sebelumnya akan hilang atau ditimpa dengan class yg baru pada setAttribute
+const judul = document.getElementsByTagName('h1')[0];
+judul.setAttribute('name', 'alya');
+
+const a = document.querySelector('section#a a'); //a yg ada di dalam section atau id a
+
+// code dibawah ini bisa dijalankan di console inspect
+// element.getAttribute()
+// element.removeAttribute()
