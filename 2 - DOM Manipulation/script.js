6551b5a66a49ae6954f8b5ae8265d4708f935b6c
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-24 21:21:09 +0700

    element.classList

--- 2 - DOM Manipulation/script.js
+++ 2 - DOM Manipulation/script.js
@@ -25,3 +25,19 @@ const a = document.querySelector('section#a a'); //a yg ada di dalam section ata
 // code dibawah ini bisa dijalankan di console inspect
 // element.getAttribute()
 // element.removeAttribute()
+
+
+
+// element.classList -> terdiri dari:
+// element.classList.add() -> menambahkan class baru
+// element.classList.remove() -> menghilangkan class lama
+// element.classList.toggle() -> kalau misalkan sebuah elemen tdk memiliki class tententu maka akan berfungsi menambahkan, tp bila sdh punya toggle akan menghilangkan class tersebut
+// element.classList.item() -> u/ mengetahui class tertentu dalam sebuah elemen
+// element.classList.contains() -> u/ mengecek dalam sebuah elemen apakah ada class tertentu yang dicari
+// element.classList.replace() -> u/ mengganti
+
+
+
+const p2 = document.querySelector('.p2');
+// dalam console dapat di-run p2.classList dan seterusnya
+
