6c54856dff4024b7b0576468983e7dd0766e489a
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-24 21:06:30 +0700

    element.style

--- 2 - DOM Manipulation/script.js
+++ 2 - DOM Manipulation/script.js
@@ -1,7 +1,14 @@
 // section.innerHTML -> mengubah konten isi yang dipanggil
-const judul = document.getElementById('judul');
-judul.innerHTML = '<em>Alya Dhiya</em>';
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<em>Alya Dhiya</em>';
 // <em></em> untuk font italic
 
-const sectionA = document.querySelector('section#a');
-sectionA.innerHTML = 'hello world!'
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'hello world!'
+
+
+
+// element.style.<propertyCSS>
+const judul = document.querySelector('#judul');
+judul.style.color = 'lightpink';
+judul.style.backgroundColor = 'salmon';
