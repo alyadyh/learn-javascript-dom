aaad1d4e0c71bab658db560af369c777a73918fd
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-24 20:58:29 +0700

    innerHTML

---
+++ 2 - DOM Manipulation/script.js
@@ -0,0 +1,7 @@
+// section.innerHTML -> mengubah konten isi yang dipanggil
+const judul = document.getElementById('judul');
+judul.innerHTML = '<em>Alya Dhiya</em>';
+// <em></em> untuk font italic
+
+const sectionA = document.querySelector('section#a');
+sectionA.innerHTML = 'hello world!'
