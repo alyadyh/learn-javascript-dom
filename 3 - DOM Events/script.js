85a6b75ab008f26ed2a6c2b47711511d88186e1c
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-25 02:05:01 +0700

    event handler on<event>

---
+++ 3 - DOM Events/script.js
@@ -0,0 +1,13 @@
+// on<event> -> event handler
+
+const p3 = document.querySelector('.p3');
+
+function ubahWarnaP3() {
+    p3.style.backgroundColor = 'lightblue';
+}
+
+function ubahWarnaP2() {
+    p2.style.backgroundColor = 'lightblue';
+}
+const p2 = document.querySelector('.p2');
+p2.onclick = ubahWarnaP2;
