67ab37b7093bed58443f0ca42f93afa11d6a9748
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-24 21:04:44 +0700

    notes

--- Test/script.js
+++ Test/script.js
@@ -1,1 +1,18 @@
 console.log(document);
+
+// Notes:
+// How  to distinguish id, class and html elements
+// HTML Elements: directly by their tag name. 
+// For example:
+// p { }
+// div { }
+
+// Classes:  by using a period (.)
+// For example:
+// .my-class { }
+// .other-class { }
+
+// IDs: by using a hash (#) followed by the ID name
+// For example:
+// #my-id { }
+// #other-id { }
