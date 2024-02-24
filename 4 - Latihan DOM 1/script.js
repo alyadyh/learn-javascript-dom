786c795580f6bd33307fd0b88b159eb75aa43618
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-25 02:48:55 +0700

    slider untuk ubah bg color

--- 4 - Latihan DOM 1/script.js
+++ 4 - Latihan DOM 1/script.js
@@ -17,3 +17,38 @@ tAcakWarna.addEventListener('click', function() {
     const b = Math.round(Math.random() * 255 + 1);
     document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')';
 });
+
+
+
+const sliderMerah = document.querySelector('input[name=sliderMerah]');
+const sliderHijau = document.querySelector('input[name=sliderHijau]');
+const sliderBiru = document.querySelector('input[name=sliderBiru]');
+
+// sliderMerah.addEventListener('change', function() {
+//     const red = sliderMerah.value;
+//     document.body.style.backgroundColor = 'rgb('+ red +', 100, 100)';
+// });
+
+// change -> tidak realtime
+// input -> berubah secara realtime
+
+sliderMerah.addEventListener('input', function() {
+    const mer = sliderMerah.value;
+    const ijo = sliderHijau.value;
+    const bir = sliderBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ mer +', '+ ijo +', '+ bir +')';
+});
+
+sliderHijau.addEventListener('input', function() {
+    const mer = sliderMerah.value;
+    const ijo = sliderHijau.value;
+    const bir = sliderBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ mer +', '+ ijo +', '+ bir +')';
+});
+
+sliderBiru.addEventListener('input', function() {
+    const mer = sliderMerah.value;
+    const ijo = sliderHijau.value;
+    const bir = sliderBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ mer +', '+ ijo +', '+ bir +')';
+});
