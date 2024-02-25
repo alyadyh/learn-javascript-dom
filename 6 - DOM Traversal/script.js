cea647206a39775fc2f06136cc362a20151c9d0f
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-02-25 20:03:34 +0700

    when use event handling

---
+++ 6 - DOM Traversal/script.js
@@ -0,0 +1,8 @@
+// Event Handling
+
+const close = document.querySelector('.close');
+const card = document.querySelector('.card');
+
+close.addEventListener('click', function() {
+    card.style.display = 'none';
+});
